9cb0e04d2b88cdfd7bd4cc7005976929c9f3713d
Edgar Cabrera <edgar@cafeinacode.com>
2015-05-24 15:57:22 -0500

    Add logic for persisting user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def spotify
     # Logic for creating user
-    spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    spotify_user.email
+    sp_user = RSpotify::User.new(auth_hash)
+    
+    user = User.where(uid: sp_user.id,
+                      email: sp_user.email,
+                      provider: auth_hash[:provider]).first_or_create
+
+    session[:authenticated] = true
+    session[:spotify_hash] = sp_user.to_hash
+    session[:user_id] = user.id
 
     redirect_to root_path
   end
 
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user
+    if session[:authenticated]
+      User.find(session[:user_id])
+    end
+  end
+
+  def spotify_user
+    if session[:authenticated]
+      RSpotify::User.new(session[:spotify_hash])
+    end
+  end
 end

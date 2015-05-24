9e73128f969043462aae82ef618fc6fbef4e9ba3
Omar Torres <torres.omar.eduardo@gmail.com>
2015-05-24 03:35:49 -0500

    Clean venue songifier component

--- app/assets/javascripts/components/venue_songifier.js.jsx
+++ app/assets/javascripts/components/venue_songifier.js.jsx
@@ -2,9 +2,22 @@ var React = require('react');
 var GoogleMap = require('./google-map');
 
 var Landing = React.createClass({
+    getInitialState: function(){
+        return {
+            latitud: 19.271752,
+            longitud: -99.161944,
+            zoom: 12
+        }
+    },
     render: function(){
         return(
-            <h1>Hey babe</h1>
+            <div style={{height: '100%', width: '100%'}}>
+                <GoogleMap latitud={Number(this.state.latitud)} longitud={Number(this.state.longitud)}
+                    zoom={[Number(this.state.zoom), 5, 21]}  ref='map'>
+                </GoogleMap>
+                <div className='inside-map-container'>
+                </div>
+            </div>
         )
     }
 });

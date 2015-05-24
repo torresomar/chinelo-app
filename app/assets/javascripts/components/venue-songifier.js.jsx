40fbfb684a2fdc0595869f7e5eabffad1f744275
Omar Torres <torres.omar.eduardo@gmail.com>
2015-05-24 05:11:06 -0500

    Rename venue songifier component

--- app/assets/javascripts/components/venue-songifier.js.jsx
+++ app/assets/javascripts/components/venue-songifier.js.jsx
@@ -1,24 +1,36 @@
 var React = require('react');
 var GoogleMap = require('./google-map');
 
+// Mixins
+var SidebarBase = require('../mixins/side-bar');
+
 var Landing = React.createClass({
     getInitialState: function(){
         return {
             latitud: 19.271752,
-            longitud: -99.161944,
-            zoom: 12
+    longitud: -99.161944,
+    zoom: 12
         }
     },
     render: function(){
         return(
             <div style={{height: '100%', width: '100%'}}>
                 <GoogleMap latitud={Number(this.state.latitud)} longitud={Number(this.state.longitud)}
-                    zoom={[Number(this.state.zoom), 5, 21]}  ref='map'>
+                zoom={[Number(this.state.zoom), 5, 21]}  ref='map'>
                 </GoogleMap>
-                <div className='inside-map-container'>
-                </div>
+                <VenueBar side={'left'} width={'250'} show={true} top={0}/>
             </div>
-        )
+            )
+    }
+});
+
+var VenueBar = React.createClass({
+    mixins: [SidebarBase],
+    render: function(){
+        return(
+            <div className="venue-sidebar" style={this.baseStyles()}>
+            </div>
+            )
     }
 });
 

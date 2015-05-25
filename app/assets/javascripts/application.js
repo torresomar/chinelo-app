782b381dd1f5e5ff329083560a626f85426bd118
Omar Torres <torres.omar.eduardo@gmail.com>
2015-05-24 22:05:59 -0500

    Start using API for venues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require paths
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks

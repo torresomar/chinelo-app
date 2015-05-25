cc84ac020bd49239150c02fd11779942caebfe8f
Edgar Cabrera <edgar@cafeinacode.com>
2015-05-24 23:54:49 -0500

    Create custom show view for location at admin

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -18,4 +18,58 @@ ActiveAdmin.register Location do
 
     actions
   end
+
+  show title: :building do
+    columns do
+      column do
+        panel 'Fotografia' do
+          image_tag(location.imageurl, width: 500, height: 300)
+        end
+        panel 'Detalles' do
+          attributes_table_for location do
+            row('Dirección') { |l| l.address }
+            row('Latitud') { |l| l.latitude }
+            row('Longitud') { |l| l.longitude }
+          end
+        end
+      end
+
+      column do
+        panel 'Resumen' do
+          table do
+            tr do
+              th 'Propiedad'
+              th 'Cantidad'
+            end
+            tr do
+              td 'Usuarios'
+              td location.user_count
+            end
+            tr do
+              td 'Canciones agregadas'
+              td location.song_count
+            end
+          end
+        end
+
+        panel 'Canciones' do
+          table do
+            tr do
+              th 'Título'
+              th 'Apariciones'
+            end
+
+            songs = location.users.flat_map(&:songs).uniq
+
+            songs.map do |song|
+              tr do
+                td song.name
+                td song.user_count(location.id)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,8 @@
 class Song < ActiveRecord::Base
   has_and_belongs_to_many :users
 
-  def user_count
-    users.size
+  def user_count(location=nil)
+    return users.size unless location
+    users.where(location_id: location).count
   end
 end

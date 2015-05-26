f22f9d039bd4c4867ea07365992054f56ee6826f
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-05-26 10:27:23 -0500

    Add DnD functionality to application

--- app/assets/javascripts/components/playlist-creator.js.jsx
+++ app/assets/javascripts/components/playlist-creator.js.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var _ = require('lodash');
 
 var PlayListCreator = React.createClass({
     componentDidMount: function(){
@@ -47,21 +48,28 @@ var InteractionContainer = React.createClass({
             artistSongs: []
         }
     },
+    addSongToUserPlayList: function(song){
+        var state = this.state;
+        var playListSongs = state.playListSongs;
+        var repeated = _.find(playListSongs, {'id': +song.id});
+        console.log(repeated);
+        if(typeof repeated === 'undefined'){
+            playListSongs.push(song);
+        }else{
+            alert("That Song is already in your playlist");
+            return;
+        }
+        this.setState({ playListSongs: playListSongs});
+    },
     handleDrop: function(e) {
         e.target.style.borderWidth = "1px";
         e.stopPropagation();
-        var songs = this.state.artistSongs,
-        item = null,
-        itemId = e.dataTransfer.getData('text/plain');
-        for (var i = 0; i < products.length; i++) {
-            if (products[i].id == itemId) {
-                item = products[i];
-            }
-        }
-        this.addItemToCart(item);
+        var songs = this.state.artistSongs;
+        var songId = e.dataTransfer.getData('text/plain');
+        var song = _.find(songs, {'id': +songId});
+        this.addSongToUserPlayList(song);
     },
     handleDragStart: function(e) {
-        console.log(e);
         var target = e.target;
         e.dataTransfer.effectAllowed = 'copy';
         e.dataTransfer.setData('text/plain', target.dataset.item);
@@ -86,24 +94,97 @@ var InteractionContainer = React.createClass({
             <div style={{width: '100%', height:'calc(100% - 59px)'}}>
                 <VenueArtistDisplay/>
                 <ArtistPlaylist songs={this.state.artistSongs} handleDragStart={this.handleDragStart} />
-                <UserPlayList handleDrop={this.handleDrop}/>
+                <UserPlayList handleDrop={this.handleDrop} songs={this.state.playListSongs}/>
             </div>
             )
     }
 });
 var UserPlayList = React.createClass({
+    getInitialState: function() {
+        return {
+            borderWidth: 1
+        }
+    },
+    handleDragEnter: function(e) {
+        this.setState({ borderWidth: 2 });
+    },
+    handleDragLeave: function(e) {
+        this.setState({ borderWidth: 1 });
+    },
+    handleDragOver: function(e) {
+        if (e.preventDefault) {
+            e.preventDefault(); // allows us to drop
+        }
+        e.dataTransfer.dropEffect = 'copy';
+    },
     render: function(){
+        var style = {
+            width: '100%',
+            height: 'calc(100% - 70px)',
+            border: this.state.borderWidth + "px dashed white",
+            borderRadius: '5px'
+        };
+        var props = this.props;
+        var length = props.songs.length;
+        var songsComponents = new Array(length);
+        if (length !== 0) {
+            var songs = props.songs;
+            while (length--) {
+                song = songs[length];
+                songsComponents[length] = <ArtistSong key={song.id} {...song} drag={props.handleDragStart}/>
+            }
+        }
         return(
-            <div style={{width: '25%',height: '100%', float: 'left', background: '#2F2F2F'}}>
+            <div style={{width: '25%',height: '100%', float: 'left', background: '#2F2F2F', padding: '20px'}}>
+                <div style={{height: '70px'}}>
+                    <h2 style={{color: '#fff', margin: '0', lineHeight: '35px'}}>
+                        CREATE THE PLAYLIST YOU WANT TO HEAR
+                    </h2>
+                </div>
+                <div onDragOver={this.handleDragOver}
+                    onDragEnter={this.handleDragEnter}
+                    onDragLeave={this.handleDragLeave} 
+                    onDrop={this.props.handleDrop}
+                    style={style}>
+                    {songsComponents}
+                </div>
             </div>
             )
     }
 });
+
+var PlayListSong = React.createClass({
+    getDefaultProps: function(){
+        return{
+            artist: 'Mana',
+            duration: '30s',
+            album: 'Cama Incendiada',
+            albumImage: 'http://placehold.it/150'
+        }
+    },
+    render: function(){
+
+        return (
+            <div className='playlist-song' style={{width: '100%', height: '80px', marginBottom: '5px'}}>
+                <div className='song-info' style={{width:'calc(100%)',height:'80px'}}>
+                    <div style={{width: '50px', float: 'left'}}>
+                        <img style={{width:'80px',height:'80px'}} src={props.albumImage} className='img-responsive'/>
+                    </div>
+                    <div style={{width: 'calc(100% - 80px)', float: 'left', paddingLeft: '5px'}}>
+                        <p style={{color:'#fff', margin: '0'}}>{props.name}<small style={{color:'#fff'}}>{' '+props.artist}</small></p>
+                        <p style={{color:'#E0E0E0', margin: '0'}}>{props.album}<small style={{color:'#E0E0E0'}}>{' '+props.artist}</small></p>
+                    </div>
+                </div>
+            </div>
+            )
+    }
+});
+
 var VenueArtistDisplay = React.createClass({
     render: function(){
         return(
             <div style={{width: '50%',height: '100%', float: 'left', position: 'relative'}}>
-                <img src="https://scontent-lax1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/11017670_10155291356625188_1245432389474863937_n.jpg?oh=a0ee4be0f9479c5d7f36205a27750061&oe=55C0B629" style={{width:'100%', height:'100%', position: 'absolute', top: '0', left: '0'}}/>
+                <img src="https://scontent-lax1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/11017670_10155291356625188_1245432389474863937_n.jpg?oh=a0ee4be0f9479c5d7f36205a27750061&oe=55C0B629" style={{width:'100%', height:'100%', position: 'absolute', top: '0', left: '0', opacity: '0.2'}}/>
                 <img src="http://ecx.images-amazon.com/images/I/91k0SfKvlLL._SL1500_.jpg" style={{width:'500px', height:'auto', position: 'absolute',
                     top:'calc(50% - 250px)',
                     left:'calc(50% - 250px)',
@@ -126,14 +207,16 @@ var ArtistSong = React.createClass({
     render: function(){
         var props = this.props;
         return (
-            <div className='artist-song' style={{width: '100%', height: '50px', marginBottom: '5px'}} onDragStart={this.props.handleDragStart}>
-                <div style={{width:'50px',height:'50px', float: 'left'}}>
-                    <img src={props.albumImage} className='img-responsive' />
-                </div>
-                <div className='song-info' style={{width:'calc(100% - 50px)',height:'50px', float: 'left'}}>
-                    <h3 style={{color:'#fff', margin: '0'}}>{props.name}<small style={{color:'#fff'}}>{' '+props.artist}</small></h3>
-                    <hr style={{margin: '0'}}/>
-                    <h4 style={{color:'#E0E0E0', margin: '0'}}>{props.album}<small style={{color:'#E0E0E0'}}>{' '+props.artist}</small></h4>
+            <div className='artist-song' style={{width: '100%', height: '60px', marginBottom: '5px'}} draggable="true"
+                data-item={props.id} onDragStart={this.props.drag}>
+                <div className='song-info' style={{width:'calc(100%)',height:'60px'}}>
+                    <div style={{width: '50px', float: 'left'}}>
+                        <img style={{width:'50px',height:'50px'}} src={props.albumImage} className='img-responsive'/>
+                    </div>
+                    <div style={{width: 'calc(100% - 50px)', float: 'left', paddingLeft: '5px'}}>
+                        <p style={{color:'#fff', margin: '0'}}>{props.name}<small style={{color:'#fff'}}>{' '+props.artist}</small></p>
+                        <p style={{color:'#E0E0E0', margin: '0'}}>{props.album}<small style={{color:'#E0E0E0'}}>{' '+props.artist}</small></p>
+                    </div>
                 </div>
             </div>
             )
@@ -149,7 +232,7 @@ var ArtistPlaylist = React.createClass({
             var songs = props.songs;
             while (length--) {
                 song = songs[length];
-                songsComponents[length] = <ArtistSong key={song.id} {...song}/>
+                songsComponents[length] = <ArtistSong key={song.id} {...song} drag={props.handleDragStart}/>
             }
         }
         return(
@@ -157,7 +240,7 @@ var ArtistPlaylist = React.createClass({
                 <div id='header-artist-playlist' style={{height: '75px',padding: '0px 20px'}}>
                     <h2 style={{color:'#fff',margin:'0',lineHeight:'75px'}}> Select your favorite songs </h2>
                 </div>
-                <div className='mCustomScrollbar' data-mcs-theme='light' style={{height:'calc(100% - 75px)',padding: '20px 0 20px 20px'}}>
+                <div className='mCustomScrollbar' data-mcs-theme='light-thick' style={{height:'calc(100% - 75px)',padding: '20px 0 20px 20px',listStyle: ''}}>
                     {songsComponents}
                 </div>
             </div>

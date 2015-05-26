edd437268cd5a11fed110c7b753cb18f2292818c
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-05-26 13:35:28 -0500

    Add authenticate component to application

---
+++ app/assets/javascripts/components/authenticate-component.js.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var AuthenticateComponent = React.createClass({
+    render: function(){
+        return null;
+    }
+});
+
+module.exports = AuthenticateComponent;

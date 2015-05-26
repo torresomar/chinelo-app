493b5a0d8f9e1a03c3382238fcbc23e2a18d4553
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-05-25 20:36:32 -0500

    Add button to update user location into venue item

--- app/assets/javascripts/components/venue-item.js.jsx
+++ app/assets/javascripts/components/venue-item.js.jsx
@@ -24,23 +24,8 @@ var VenueItem = React.createClass({
                         <a href="#" id="1">
                             <h3>{this.props.building}</h3>
                         </a>
-                        <figure>{this.props.address}</figure>
-                        <div className="price">$4000</div>
-                        <div className="info">
-                            <div className="type">
-                                <i><img src={IMAGES['umbrella']} className="mCS_img_loaded"/>2</i>
-                                <span>Apartment</span>
-                            </div>
-                            <div className="rating" data-rating="4">
-                                <span className="stars">
-                                    <i className="fa fa-star s1 active" data-score="1"></i>
-                                    <i className="fa fa-star s2 active" data-score="2"></i>
-                                    <i className="fa fa-star s3 active" data-score="3"></i>
-                                    <i className="fa fa-star s4 active" data-score="4"></i>
-                                    <i className="fa fa-star s5" data-score="5"></i>
-                                </span>
-                            </div>
-                        </div>
+                        <figure style={{height:'75px'}}>{this.props.address}</figure>
+                        <div className="price" style={{width:'100%'}}>SET MY LOCATION</div>
                     </div>
                 </div>
             </li>
